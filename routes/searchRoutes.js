e8b5371988d0c98f58fd9b46ed63d00877fa11d9
arsalan <arsalanrashid285@gmail.com>
2025-11-19 16:55:50 +0530

    multi-field search added

--- routes/searchRoutes.js
+++ routes/searchRoutes.js
@@ -2,11 +2,19 @@ const express = require("express");
 const router = express.Router();
 const Case = require("../models/Case");
 
+// SEARCH IN ALL IMPORTANT FIELDS
 router.get("/search", async (req, res) => {
     const query = req.query.query;
 
+    if (!query) return res.json([]);
+
     const results = await Case.find({
-        CaseNumber: { $regex: query, $options: "i" }
+        $or: [
+            { CaseNumber: { $regex: query, $options: "i" } },
+            { Parties: { $regex: query, $options: "i" } },
+            { PetitionerAdvocate: { $regex: query, $options: "i" } },
+            { RespondentAdvocate: { $regex: query, $options: "i" } }
+        ]
     });
 
     res.json(results);
